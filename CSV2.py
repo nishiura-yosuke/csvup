bc00c622ee6975417db2c8b8a64a3c92fad2ee36
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-16 15:55:39 +0900

    rr

--- CSV2.py
+++ CSV2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import japanize_matplotlib
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
